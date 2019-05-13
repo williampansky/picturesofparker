86ac074140a2cfd59e49c6c32147f2c395a326d0
WIlliam Pansky <williampansky@WIlliams-MacBook-Pro.local>
2019-05-12 19:55:54 -0500

    Changed netlify to REACT_APP_ to try it

--- src/views/TheHome.vue
+++ src/views/TheHome.vue
@@ -70,7 +70,7 @@ export default {
         if (process.env.NODE_ENV === 'development')
             this.apiKey = process.env.VUE_APP_APIKEY;
         else
-            this.apiKey = process.env.VUE_APP_FLICKR_KEY;
+            this.apiKey = process.env.REACT_APP_FLICKR_KEY;
     },
 
     computed: {
@@ -116,8 +116,8 @@ export default {
                 key = process.env.VUE_APP_APIKEY;
                 user = process.env.VUE_APP_APIUSER;
             } else {
-                key = process.env.VUE_APP_FLICKR_KEY;
-                user = process.env.VUE_APP_FLICKR_USER;
+                key = process.env.REACT_APP_FLICKR_KEY;
+                user = process.env.REACT_APP_FLICKR_USER;
             }
 
             this.$axios
