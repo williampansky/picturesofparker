9cf385553cdb54a4948e8b7dc68168ccb664aca6
WIlliam Pansky <williampansky@WIlliams-MacBook-Pro.local>
2019-05-19 10:22:55 -0500

    created Grid.vue

---
+++ src/components/Grid.vue
@@ -0,0 +1,32 @@
+<template>
+    <div
+    :uk-grid="masonry ? 'masonry:true;' : ''"
+    :class="[
+        size ? 'uk-grid-' + size : '',
+        match ? 'uk-grid-match' : ''
+    ]"
+    class="uk-grid
+    uk-child-width-1-2
+    uk-child-width-1-3@m
+    uk-child-width-1-4@l
+    uk-child-width-1-6@xl">
+        <slot></slot>
+    </div>
+</template>
+
+
+<script>
+/**
+ * @module Grid
+ * @see [UIkit]{@link https://getuikit.com/docs/grid}
+ */
+export default {
+    name: 'Grid',
+    props: {
+        childwidths: { type: Array, default: () => ([]) },
+        masonry: { type: Boolean, default: false },
+        match: { type: Boolean, default: false },
+        size: { type: String, default: 'default' }
+    }
+};
+</script>
