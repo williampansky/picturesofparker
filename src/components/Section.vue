7944cad0c2b74d105c9786e5ed0336c105a3dfe0
WIlliam Pansky <williampansky@WIlliams-MacBook-Pro.local>
2019-05-19 10:23:21 -0500

    created Section.vue

---
+++ src/components/Section.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="uk-section uk-section-small">
+        <slot></slot>
+    </section>
+</template>
+
+
+<script>
+/**
+ * @module Section
+ * @see [UIkit]{@link https://getuikit.com/docs/section}
+ */
+export default {
+    name: 'Section'
+};
+</script>
+
+
+<style lang="scss" scoped>
+.uk-section-small {
+    padding: 15px 0;
+
+    @include breakpoint('small') {
+        padding: 30px 0;
+    }
+
+    @include breakpoint('medium') {
+        padding: 40px 0;
+    }
+}
+</style>
